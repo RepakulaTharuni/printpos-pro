f65188385b36d64714e4817bfc8d075501def082
Rithika Shankar <rithikas4002@gmail.com>
2024-11-08 23:29:49 +0530

    Delete backend directory

--- backend/src/main/java/com/printpos/controller/PrintController.java
+++
@@ -1,9 +0,0 @@
-@RestController
-@RequestMapping("/api")
-public class PrintController {
-    @PostMapping("/print")
-    public ResponseEntity<String> printPrescription(@RequestBody Map<String, Object> data) {
-        // Call printer service here
-        return ResponseEntity.ok("Printed successfully");
-    }
-}
